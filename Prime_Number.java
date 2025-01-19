c845e016387e550c0cad110528e218c2698d1094
HarshadaM0111 <harshadamane0111@gmail.com>
2025-01-19 15:26:00 +0530

    Add files via upload

---
+++ Prime_Number.java
@@ -0,0 +1,51 @@
+package Package_Java;
+
+/* Write a java program to check given number is perfect number or not  */
+
+import java.util.Scanner;
+public class Prime_Number {
+
+	public static boolean isPrime(int num) { 
+		if (num == 1) {     
+			return false;
+		}
+
+		for (int i = 2; i <=  Math.sqrt(num); i++) {
+			// If num is divisible by any number, it is not prime
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		// If no divisors are found, the number is prime
+		return true;
+	}
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in); //  take input from the user
+
+		System.out.print("Enter a number: "); // User Enter a number
+		int number = scanner.nextInt();
+
+		// Check if the number is prime OR not
+		if (isPrime(number)) {
+			System.out.println(number + " is a prime number.");
+		} else {
+			System.out.println(number + " is not a prime number.");
+		}
+
+	}    
+
+}
+
+
+// OUTPUT
+
+/*
+ * Enter a number: 2
+   2 is a prime number.
+
+ * Enter a number: 4
+   4 is not a prime number.   
+ */
+
+
